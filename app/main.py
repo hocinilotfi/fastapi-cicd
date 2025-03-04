77b379b0b2202bd1672d074917181fae4d7d6f7c
Lotfi Hocini <lotfi.hocini@hotmail.com>
2025-03-04 13:36:43 +0100

    add login end point

--- app/main.py
+++ app/main.py
@@ -4,3 +4,7 @@ app = FastAPI()
 @app.get('/')
 async def root():
     return {'message': 'Hello World from continus deployment'}
+
+@app.get('/login')
+async def method_name():
+    return {'message': 'Login page'}
