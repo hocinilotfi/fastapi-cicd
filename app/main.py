d1d5e00c9018e04cd6d92615311fd6ccc7040510
Lotfi Hocini <lotfi.hocini@hotmail.com>
2025-03-11 21:41:40 +0100

    first

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = FastAPI()
 
 @app.get('/')
 async def root():
-    return {'message': 'Hello World from continus deployment - test de deployment failed'}
+    return {'message': 'Hello World from continus deployment - test de deployment'}
 
 @app.get('/login')
 async def method_name():
