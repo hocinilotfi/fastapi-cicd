fb8546b396e64149efdec8b2c56471ed7b03aaff
Lotfi Hocini <lotfi.hocini@hotmail.com>
2025-03-11 21:03:42 +0100

    first

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = FastAPI()
 
 @app.get('/')
 async def root():
-    return {'message': 'Hello World from continus deployment'}
+    return {'message': 'Hello World from continus deployment with aymen'}
 
 @app.get('/login')
 async def method_name():
