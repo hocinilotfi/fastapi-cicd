addb75dc29ff868f160b9313c92cf23c8719a89c
Lotfi Hocini <lotfi.hocini@hotmail.com>
2025-03-13 13:58:05 +0100

    first

--- app/main.py
+++ app/main.py
@@ -4,11 +4,3 @@ app = FastAPI()
 @app.get('/')
 async def root():
     return {'message': 'Hello World from continus deployment - test de deployment'}
-
-@app.get('/login')
-async def method_name():
-    return {'message': 'Login page modified'}
-
-@app.get('/signup')
-async def method_name():
-    return {'message': 'Signup page'}
